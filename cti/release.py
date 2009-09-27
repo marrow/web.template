aff08721779363e90e57e1c88ad211818f2a68bb
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-09-26 17:27:39 -0700

    Added release information.

---
+++ cti/release.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""Release information about the Common Templating Interface."""
+
+
+name = "TemplatingInterface"
+version = "0.1"
+
+summary = "A common templating interface for Python applications."
+description = """"""
+author = "Alice Bevan-McGregor"
+email = "alice+cti@gothcandy.com"
+url = "http://github.com/GothAlice/TemplatingInterface/"
+download_url = "http://cheeseshop.python.org/pypi/TemplatingInterface/"
+copyright = "© 2009 Alice Bevan-McGregor"
+license = "MIT"
