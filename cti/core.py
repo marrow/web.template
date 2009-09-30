e1b2932340e88aa2b174d38adae2f96ecb24416a
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-09-30 06:54:07 -0700

    Added EngineProxy caching class, added support for class usage (instead of bare function), and added defaulting behavior.

--- cti/core.py
+++ cti/core.py
@@ -6,6 +6,26 @@ import pkg_resources
 __all__ = ['Engines']
 
 
+
+class EngineProxy(object):
+    def __init__(self, engine, defaults=dict()):
+        self.engine = engine
+        self.defaults = defaults
+        self.cached = None
+    
+    def __call__(self, *args, **kw):
+        options = dict(self.defaults)
+        options.update(kw)
+        
+        if isinstance(self.engine, pkg_resources.EntryPoint):
+            self.engine = self.engine.load()
+        
+        if isinstance(self.engine, type):
+            self.engine = self.engine(**options)
+        
+        return self.engine(*args, **options)
+
+
 class Engines(dict):
     """Engines is a dictionary subclass that allows easy reference to
     rendering engines.
@@ -21,29 +41,31 @@ class Engines(dict):
     at runtime.
     """
     
-    def __init__(self):
+    def __init__(self, **kw):
+        super(Engines, self).__init__()
+        self.refresh()
+    
+    def refresh(self, **kw):
+        print "refresh"
         engines = []
         
         for engine in pkg_resources.iter_entry_points('web.templating'):
-            engines.append((engine.name, engine))
+            engines.append((engine.name, EngineProxy(engine, kw.get(engine.name, dict()))))
         
-        super(Engines, self).__init__(engines)
+        self.clear()
+        self.update(engines)
     
-    def __getitem__(self, name):
-        engine = super(Engines, self).__getitem__(name)
-        return engine.load()
+    def __setitem__(self, name, value):
+        if not callable(value):
+            raise TypeError("Engines must be callable, a bare function, method, or callable class instance.")
+        
+        if issubclass(value, EngineProxy):
+            return super(Engines, self).__setitem__(EngineProxy(value))
+        
+        super(Engines, self).__setitem__(EngineProxy(value))
     
     def __getattr__(self, name):
         return self[name]
     
-    def __missing__(self, name):
-        engines = []
-        
-        for engine in pkg_resources.iter_entry_points('web.templating'):
-            engines.append((engine.name, engine))
-        
-        self.clear()
-        self.update(engines)
-        
-        if name not in dict(engines): raise KeyError
-        return self[name]
+    def __setattr__(self, name, value):
+        self[name] = value
