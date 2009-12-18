ca7753dfe6f3e45274c657ee08d1c851ba6138bd
Alice Bevan-McGregor <alice.mcgregor@me.com>
2009-12-17 19:18:47 -0800

    Updated the Mako engine to include a TemplateLookup loader for template inheritance support.

--- cti/templating/mako_.py
+++ cti/templating/mako_.py
@@ -1,9 +1,12 @@
 # encoding: utf-8
 
+from os import path
+
 from cti.core import Engine
 
 try:
     from mako.template import Template
+    from mako.lookup import TemplateLookup
 
 except ImportError:
     raise ImportError('You must install the mako package.')
@@ -15,7 +18,18 @@ __all__ = ['Mako']
 
 class Mako(Engine):
     def load(self, filename, **options):
-        return Template(filename=filename)
+        bpath = path.dirname(filename)
+        
+        try:
+            loader = self.cache[bpath]
+        
+        except KeyError:
+            loader = self.cache[bpath] = TemplateLookup(
+                    directories=[bpath],
+                    filesystem_checks=self.monitor
+                )
+        
+        return Template(filename=filename, lookup=loader)
     
     def render(self, template, data, **options):
         return self.mimetype, template.render_unicode(**data)
