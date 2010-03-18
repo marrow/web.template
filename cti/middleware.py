72c1f774f547fddf930b9f363ea61e21ed463f0c
Alice Bevan-McGregor <alice.mcgregor@me.com>
2010-03-18 02:51:20 -0700

    Added compat for the middleware.

---
+++ cti/middleware.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.warn(
+        'The Common Template Interface middleware has been deprecated and moved into WebCore.\n'
+        'Update your imports to reference "web.extras.templating" instead.',
+        DeprecationWarning
+    )
+
+import web.extras.templating
+from web.extras.templating import *
+
+__all__ = webcore.extras.templating.__all__
