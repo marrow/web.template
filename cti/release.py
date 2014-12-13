3ac7c80bc5cd5733a6f40b63cc315da7ce634b02
Alice Bevan–McGregor <alice@gothcandy.com>
2014-12-13 01:22:37 -0500

    Version bump and removal of legacy cruft.

--- cti/release.py
+++
@@ -1,8 +0,0 @@
-import warnings
-warnings.warn(
-        'Access to the common template interface via the "cti" package has been deprecated.\n'
-        'Update your imports to reference "marrow.templating" instead.',
-        DeprecationWarning
-    )
-
-from marrow.templating.release import *
